37a41679deeabbcec8cf5f9ff66a98e067194c6b
Adil Shehzad <adil.shehzad4146@gmail.com>
2022-06-10 00:04:46 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 provider "aws" {
   region                  = "us-east-1"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "spacelift"
+  
 }
 
 resource "aws_instance" "new_aws_instance"{
