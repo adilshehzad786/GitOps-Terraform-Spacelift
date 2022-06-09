5c9553cce21a4012c308dccafc9edda2b83f6925
Adil <adil@adil.com>
2022-06-09 18:47:53 +0000

    Adding configurtions

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
+            access_key = AWS_ACCESS_KEY_ID
+            secret_key = AWS_SECRET_ACCESS_KEY
+            region     = us-east-1
         }
     }
 }
